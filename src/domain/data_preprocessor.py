b3f80529e6123fb0fb46cec760516b538beb4345
kovanostra <kovanostra@gmail.com>
2020-04-26 13:09:49 +0200

    Refactored data preprocessor and preprocessor classes

--- src/domain/data_preprocessor.py
+++ src/domain/data_preprocessor.py
@@ -2,7 +2,6 @@ import logging
 from typing import Any, Tuple, List
 
 import torch as to
-from math import ceil
 from torch.utils.data import DataLoader
 
 from src.domain.graph import Graph
@@ -43,35 +42,6 @@ class DataPreprocessor(Preprocessor):
         validation_index = int((1 - validation_split - test_split) * len(raw_dataset))
         test_index = int((1 - test_split) * len(raw_dataset))
         return test_index, validation_index
-
-    def preprocess(self, dataset: Any, batches: int) -> Any:
-        self.get_logger().info("Started preprocessing data")
-        batch_length = self._calculate_batch_length(len(dataset), batches)
-        dataset_in_batches = []
-        for batch_index in range(0, len(dataset), batch_length):
-            batch_is_complete = False
-            batch = []
-            data_index = batch_index
-            while not batch_is_complete:
-                batch.append(Graph(dataset[data_index][1], dataset[data_index][0]))
-                data_index += 1
-                if len(batch) == batch_length or data_index == len(dataset):
-                    batch_is_complete = True
-            dataset_in_batches.append(batch)
-        self.get_logger().info("Finished preprocessing data into " + str(len(dataset_in_batches)) + " batches")
-        return dataset_in_batches
-
-    @staticmethod
-    def _calculate_batch_length(dataset_length: int, batches: int) -> int:
-        if batches <= 1:
-            batch_length = dataset_length
-        elif dataset_length / 2 <= batches < dataset_length:
-            batch_length = 2
-        elif batches >= dataset_length:
-            batch_length = 1
-        else:
-            batch_length = ceil(dataset_length / batches)
-        return batch_length
 
     @staticmethod
     def get_logger() -> logging.Logger:
