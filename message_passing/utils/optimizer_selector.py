062eecc97b5acef1e8f5e5179fc0c78b4ada97ec
kovanostra <kovanostra@gmail.com>
2020-05-01 11:51:12 +0200

    Moved loss functions and optimizers to utils

--- message_passing/utils/optimizer_selector.py
+++ message_passing/utils/optimizer_selector.py
@@ -2,7 +2,7 @@ import logging
 
 from torch.optim.optimizer import Optimizer
 
-from message_passing.fixtures.optimizers import optimizers
+from message_passing.utils.optimizers import optimizers
 
 
 class OptimizerSelector:
