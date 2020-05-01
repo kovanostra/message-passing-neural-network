f0ee5c99594e6d5ed6e1114fe9e596a83b9de95e
Michail Kovanis <kovanostra@users.noreply.github.com>
2020-05-01 14:18:05 +0200

    Update setup.py
    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='message-passing-nn',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(exclude=["tests"]),
     url='https://github.com/kovanostra/message-passing-nn',
     download_url='https://github.com/kovanostra/message-passing-nn/archive/1.1.1.tar.gz',
