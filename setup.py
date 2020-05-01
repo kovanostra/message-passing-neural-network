84fc6f765dc69ead6a154af3f528c83019129af7
kovanostra <kovanostra@gmail.com>
2020-05-01 13:56:57 +0200

    setup.cfg. additions on setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ setup(
     name='message-passing-nn',
     version='1.1.1',
     packages=find_packages(exclude=["tests"]),
-    url='',
+    url='https://github.com/kovanostra/message-passing-nn',
+    download_url='',
+    keywords=[''],
     license='MIT',
     author='Michail Kovanis',
     description='A message passing neural network with GRU units',
     install_requires=[
         'click',
-        'numpy==1.17.4',
         'pandas==1.0.3',
         'torch==1.4.0'
     ],
@@ -19,4 +20,11 @@ setup(
             'message-passing-nn = message_passing_nn.cli:main'
         ],
     },
+    classifiers=[
+        'Development Status :: 3 - Beta',
+        'Intended Audience :: Data-scientists',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7.6',
+    ],
 )
