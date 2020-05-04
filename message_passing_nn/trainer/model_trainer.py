e6c1d6434a136e7cc6b3b46332dc2896df6a488a
kovanostra <kovanostra@gmail.com>
2020-05-04 09:48:36 +0200

    Fixed some warnings

--- message_passing_nn/trainer/model_trainer.py
+++ message_passing_nn/trainer/model_trainer.py
@@ -24,11 +24,12 @@ class ModelTrainer:
         node_features_size, adjacency_matrix_size, labels_size = data_dimensions
         number_of_nodes = adjacency_matrix_size[0]
         number_of_node_features = node_features_size[1]
+        fully_connected_layer_output_size = labels_size[0]
         self.model = self.model.of(time_steps=configuration_dictionary['time_steps'],
                                    number_of_nodes=number_of_nodes,
                                    number_of_node_features=number_of_node_features,
                                    fully_connected_layer_input_size=number_of_nodes * number_of_node_features,
-                                   fully_connected_layer_output_size=len(labels_size),
+                                   fully_connected_layer_output_size=fully_connected_layer_output_size,
                                    device=self.device)
         self.model.to(self.device)
         self.loss_function = self._instantiate_the_loss_function(
