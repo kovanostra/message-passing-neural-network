f1875ce197723234f5fc3a4cae121f8250a334e7
kovanostra <kovanostra@gmail.com>
2020-05-07 12:42:44 +0200

    Corrected tensor shapes and removed a not necessary matmul for GRU model

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='message-passing-nn',
-    version='1.3.1',
+    version='1.3.2',
     packages=find_packages(exclude=["tests"]),
     url='https://github.com/kovanostra/message-passing-nn',
-    download_url='https://github.com/kovanostra/message-passing-nn/archive/1.3.1.tar.gz',
+    download_url='https://github.com/kovanostra/message-passing-nn/archive/1.3.2.tar.gz',
     keywords=['MESSAGE PASSING', 'NEURAL NETWORK', 'GRU'],
     license='MIT',
     author='Michail Kovanis',
