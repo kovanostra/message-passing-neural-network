8000809c2bd72777c4b2351fd4eb2d5315697abb
kovanostra <kovanostra@gmail.com>
2020-07-10 22:40:47 +0200

    Chore: file removed

--- inference-parameters.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-export DATASET_NAME='sample-dataset'
-export DATA_DIRECTORY='data/'
-export MODEL_DIRECTORY='model_checkpoints/configuration&id__model&RNN__epochs&1000__loss_function&MSE__optimizer&SGD__batch_size&10__maximum_number_of_features&-1__maximum_number_of_nodes&30__validation_split&0.2__test_split&0.1__time_steps&1__validation_period&20/Epoch_1000_model_state_dictionary.pth'
-export RESULTS_DIRECTORY='grid_search_results'
-export MODEL='RNN'
-export DEVICE='cpu'
