20a3116a17de94ab57becd30229ef9c17a23ba30
kovanostra <kovanostra@gmail.com>
2020-07-27 11:48:44 +0300

    Refactor: The base matmul of w_node_features with node_features is performed only once.
    Other operations that need it just have to access its value.

--- message_passing_nn/utils/messages.cpp
+++ message_passing_nn/utils/messages.cpp
@@ -7,6 +7,7 @@ at::Tensor get_messages_to_all_end_nodes(const int& node_id,
                                             const at::Tensor& all_neighbors_of_node,
                                             const at::Tensor& node_features,
                                             const at::Tensor& messages_previous_step,
+                                            const at::Tensor& base_messages,
                                             at::Tensor& new_messages_of_node) {
   for (int end_node_index = 0; end_node_index<all_neighbors_of_node.sizes()[0]; end_node_index++){
       auto end_node_id = all_neighbors_of_node[end_node_index].item<int64_t>();
@@ -19,7 +20,7 @@ at::Tensor get_messages_to_all_end_nodes(const int& node_id,
           }
       }
     }
-  return new_messages_of_node;
+  return at::add(base_messages, new_messages_of_node);
 }
 
 std::vector<at::Tensor> compose_messages(
@@ -35,16 +36,17 @@ std::vector<at::Tensor> compose_messages(
   auto new_messages = at::zeros_like({messages_init});
   auto messages_previous_step = at::zeros_like({messages_init});
   auto new_messages_of_node = at::zeros({messages_previous_step.sizes()[1], messages_previous_step.sizes()[2]});
+  auto base_messages = at::matmul(w_graph_node_features, node_features);
   for (int time_step = 0; time_step<time_steps; time_step++) {
     std::swap(messages_previous_step, new_messages);
     for (int node_id = 0; node_id<number_of_nodes; node_id++) {
-      new_messages_of_node = at::matmul(w_graph_node_features, node_features);
       new_messages[node_id] = get_messages_to_all_end_nodes(node_id,
                                                             w_graph_neighbor_messages,
                                                             w_graph_node_features,
                                                             all_neighbors[node_id],
                                                             node_features,
                                                             messages_previous_step,
+                                                            base_messages,
                                                             new_messages_of_node);
       new_messages_of_node.zero_();
     }
