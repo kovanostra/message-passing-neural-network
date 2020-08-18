9e1db7ee20932ea9903454325a320adedfeab4ad
kovanostra <kovanostra@gmail.com>
2020-08-18 18:29:53 +0300

    Bug: Tests replacing the += with element1 + element2

--- message_passing_nn/graph/rnn_encoder_cuda_kernel.cu
+++ message_passing_nn/graph/rnn_encoder_cuda_kernel.cu
@@ -24,7 +24,7 @@ __global__ void compose_messages_kernel(
           for (int neighbor_index = 0; neighbor_index < all_neighbors.size(1); neighbor_index++) {
             auto neighbor = std::round(all_neighbors[node_id][neighbor_index]);
             if (neighbor >= 0 && neighbor_index!=end_node_index) {
-              new_messages[node_id][end_node_id] += base_neighbor_messages[neighbor][node_id];
+              new_messages[node_id][end_node_id] = new_messages[node_id][end_node_id] + base_neighbor_messages[neighbor][node_id];
             }
           }
         }
