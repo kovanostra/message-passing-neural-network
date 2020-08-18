b9787162f29072a9db8cec19484c9b8d9cabc36c
kovanostra <kovanostra@gmail.com>
2020-08-18 16:51:39 +0300

    Refactor: CPU and GPU implementation of message passing step are merged.
    This to incorporate the big matmul of neighbor messages.

--- message_passing_nn/utils/messages.cpp
+++ message_passing_nn/utils/messages.cpp
@@ -1,28 +1,6 @@
 #include <torch/extension.h>
 #include "../utils/messages.h"
 
-at::Tensor get_messages_to_all_end_nodes(const int& node_id,
-                                            const at::Tensor& w_graph_neighbor_messages,
-                                            const at::Tensor& w_graph_node_features,
-                                            const at::Tensor& all_neighbors_of_node,
-                                            const at::Tensor& node_features,
-                                            const at::Tensor& messages_previous_step,
-                                            const at::Tensor& base_messages,
-                                            at::Tensor& new_messages_of_node) {
-  for (int end_node_index = 0; end_node_index<all_neighbors_of_node.sizes()[0]; end_node_index++){
-      auto end_node_id = all_neighbors_of_node[end_node_index].item<int64_t>();
-      if (end_node_id >= 0) {
-        for (int neighbor_index = 0; neighbor_index<all_neighbors_of_node.sizes()[0]; neighbor_index++) {
-            auto neighbor = all_neighbors_of_node[neighbor_index].item<int64_t>();
-            if (neighbor >= 0 && neighbor_index!=end_node_index) {
-              new_messages_of_node[end_node_id] += at::matmul(w_graph_neighbor_messages, at::relu(messages_previous_step[neighbor][node_id]));
-            }
-          }
-      }
-    }
-  return at::add(base_messages, new_messages_of_node);
-}
-
 std::vector<at::Tensor> compose_messages(
     const int& time_steps,
     const int& number_of_nodes,
@@ -38,18 +16,22 @@ std::vector<at::Tensor> compose_messages(
   auto new_messages_of_node = at::zeros({messages_previous_step.sizes()[1], messages_previous_step.sizes()[2]});
   auto base_messages = at::matmul(w_graph_node_features, node_features);
   for (int time_step = 0; time_step<time_steps; time_step++) {
+    auto base_neighbor_messages = at::matmul(w_graph_neighbor_messages, at::relu(messages_previous_step));
     std::swap(messages_previous_step, new_messages);
-    for (int node_id = 0; node_id<number_of_nodes; node_id++) {
-      new_messages[node_id] = get_messages_to_all_end_nodes(node_id,
-                                                            w_graph_neighbor_messages,
-                                                            w_graph_node_features,
-                                                            all_neighbors[node_id],
-                                                            node_features,
-                                                            messages_previous_step,
-                                                            base_messages,
-                                                            new_messages_of_node);
-      new_messages_of_node.zero_();
+    for (int node_id; node_id < all_neighbors.size(0); node_id++) {
+      for (int end_node_index = 0; end_node_index < all_neighbors.size(1); end_node_index++){
+        auto end_node_id = all_neighbors[node_id][end_node_index].item<int>();
+        if (end_node_id >= 0) {
+          for (int neighbor_index = 0; neighbor_index < all_neighbors.size(1); neighbor_index++) {
+            auto neighbor = all_neighbors[node_id][neighbor_index].item<int>();
+            if (neighbor >= 0 && neighbor_index!=end_node_index) {
+              new_messages[node_id][end_node_id] += base_neighbor_messages[neighbor][node_id];
+            }
+          }
+        }
+      }
     }
+    new_messages += base_messages;
   }
   return {new_messages, messages_previous_step};
 }
